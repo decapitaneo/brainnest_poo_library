e7d917e04646a4a5e605f456883615e1f8aa7888
Denise Capitaneo <decapitaneo@gmail.com>
2023-02-24 21:27:38 -0300

    form validation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("[data-form]");
 
-let myLibrary = [];
+// let myLibrary = [];
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
@@ -28,24 +28,50 @@ function addingBookToLibrary(){
     const isbn = document.querySelector("[data-isbn]").value;
     const pages = document.querySelector("[data-pages]").value;
     const read = document.querySelector("[data-read]").value;
-    if(title === "" || author === "" || isbn === "" || pages === ""){
-        alert("preencha todos os campos")
-    }else{
+    
+    if(title === ""){
+        document.getElementById("titleValidation").innerHTML = "Please fill in the tittle";
+        
+    }else if(author === ""){
+        document.getElementById("authorValidation").innerHTML = "Please fill in the author";
+        
+    }else if(isbn === ""){
+        document.getElementById("isbnValidation").innerHTML = "Please fill in the isbn";
+    
+    }else if(pages === ""){
+        document.getElementById("pagesValidation").innerHTML = "Please fill in the pages";
+        
+    }else {
         const book = new Book(bookId, title, author, isbn, pages, read);
+        let myLibrary = localStorage.getItem("books");
+        myLibrary = JSON.parse(myLibrary); //array de verdade
         myLibrary.push(book);
+        localStorage.setItem("books", JSON.stringify(myLibrary));
+        
         loadLibraryIntoDiv();
         showAlert("Book registered with success!")
+        
+        //clear fields after 
+        document.getElementById('title').value = '';
+        document.getElementById('author').value = '';
+        document.getElementById('isbn').value = '';
+        document.getElementById('pages').value = '';
     }
 }
 
 //Localstorage -store the book locally
-
-
-
+function verifyLocalStorage(){
+    if(localStorage.getItem("books") === null){
+        //if null it will return an empty array
+        localStorage.setItem("books", "[]");
+    }
+}
 
 function loadLibraryIntoDiv() {
     const libraryDiv = document.querySelector("[data-library]");
     libraryDiv.innerHTML = "";
+    let myLibrary = localStorage.getItem("books");
+    myLibrary = JSON.parse(myLibrary);
     myLibrary.forEach(book => {
         const bookDiv = document.createElement("div");
         bookDiv.classList.add("book");
@@ -65,23 +91,31 @@ function loadLibraryIntoDiv() {
 }
 
 function changeBookReadStatus(bookId) {
+    let myLibrary = localStorage.getItem("books");
+    myLibrary = JSON.parse(myLibrary);
     const bookIndex = myLibrary.findIndex((b) => b.id === bookId);
     myLibrary[bookIndex].read = myLibrary[bookIndex].read === "Yes" ? "No" : "Yes";
+    localStorage.setItem("books", JSON.stringify(myLibrary));
     loadLibraryIntoDiv();
     showAlert("Status changed successfully!")
 }
 
 // When the delete button from a book is clicked, the book is deleted from the library
 function deleteBookFromLibrary(bookId) {
+    let myLibrary = localStorage.getItem("books");
+    myLibrary = JSON.parse(myLibrary);
     myLibrary = myLibrary.filter((b) => b.id !== bookId);
+    localStorage.setItem("books", JSON.stringify(myLibrary));
     loadLibraryIntoDiv();
     showAlert("Book deleted successfully!")
 }
 
+
 //alert message
-
-
 function showAlert(text){
     let validationMessage = document.getElementById("message");
     validationMessage.innerHTML = text;   
 }
+
+verifyLocalStorage();
+loadLibraryIntoDiv();
